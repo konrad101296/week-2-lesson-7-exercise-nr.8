e0b85d2ad993d3c2c6dce66f067fbc81bd57e98d
konrad101296 <konradslimak@o2.pl>
2024-02-29 20:36:06 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Podaj wynik z matematyki:");
+int Matma = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj wynik z fizyki:");
+int Fizyka = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj wynik z chemi:");
+int Chemia = int.Parse(Console.ReadLine());
+
+int Razem = Matma + Fizyka + Chemia;
+
+if (Matma > 70 && Fizyka > 55 && Chemia > 45 && Razem > 180)
+{
+    Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+}
+else if (Matma + Fizyka > 150 || Matma + Chemia > 150)
+{
+    Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+}
+else
+{
+    Console.WriteLine("Kandydat nie dopuszczony do rekrutacji");
+}
